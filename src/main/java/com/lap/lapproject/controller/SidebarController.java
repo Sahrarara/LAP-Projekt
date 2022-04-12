7eb6b6da26104f9240f01271afbd79841855c6f0
Sahrarara <sarah.e.git@gmail.com>
2022-04-12 11:18:01 +0200

    logout Button works for guest login

--- src/main/java/com/lap/lapproject/controller/SidebarController.java
+++ src/main/java/com/lap/lapproject/controller/SidebarController.java
@@ -1,11 +1,23 @@
 package com.lap.lapproject.controller;
 
+import com.lap.lapproject.LoginApplication;
 import com.lap.lapproject.application.Constants;
+import com.lap.lapproject.model.Admin;
+import com.lap.lapproject.model.Trainer;
+import com.lap.lapproject.model.UserModel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class SidebarController extends BaseController{
 
@@ -14,10 +26,11 @@ public class SidebarController extends BaseController{
     private Button trainerBtn;
     @FXML
     private Button logoutBtn;
+    @FXML
+    private Label nameLabel;
 
     @FXML
     void onCoreDataButtonPressed(ActionEvent event){
-        log.debug("Button pressed");
         model.setPathForDetailView(Constants.PATH_TO_FXML_DASHBOARD);
     }
 
@@ -28,8 +41,7 @@ public class SidebarController extends BaseController{
 
     @FXML
     private void onLogoutBtnClick(ActionEvent actionEvent) {
-        //TODO: clear entire Stage (Sidebar needs to be removed)
-        model.setPathForDetailView(Constants.PATH_TO_FXML_LOGIN);
+        moveToLogin();
     }
 
     @FXML
@@ -51,4 +63,31 @@ public class SidebarController extends BaseController{
     private void onLocationBtnClick(ActionEvent actionEvent) {
         model.setPathForDetailView(Constants.PATH_TO_FXML_LOCATION);
     }
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        nameLabel.setText("Gast");
+    }
+
+    private Stage getCurrentStage(){
+        return (Stage) nameLabel.getScene().getWindow();
+    }
+
+    private void moveToLogin(){
+        Stage currentStage = this.getCurrentStage();
+        currentStage.close();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource(Constants.PATH_TO_FXML_LOGIN));
+        Scene scene = null;
+
+        try {
+            scene= new Scene(fxmlLoader.load());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        currentStage.setTitle("Raum Management");
+        currentStage.setScene(scene);
+        currentStage.show();
+    }
+
 }
