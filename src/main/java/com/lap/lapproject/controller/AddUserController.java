3a1ad5ab01a0d73615d49b5331019a42f5483586
Sahrarara <sarah.e.git@gmail.com>
2022-04-14 12:35:32 +0200

    Made popup window for adding new trainer

--- src/main/java/com/lap/lapproject/controller/AddUserController.java
+++ src/main/java/com/lap/lapproject/controller/AddUserController.java
@@ -1,4 +1,43 @@
 package com.lap.lapproject.controller;
 
+import com.lap.lapproject.LoginApplication;
+import com.lap.lapproject.application.Constants;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+
 public class AddUserController {
+    @FXML
+    private Button FileBtn;
+
+    @FXML
+    private void onAbortBtnClick(ActionEvent actionEvent) {
+        closeCurrentStage();
+    }
+
+    @FXML
+    private void onAddBtnClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onFileBtnClick(ActionEvent actionEvent) {
+    }
+
+
+    private Stage getCurrentStage(){
+        return (Stage) FileBtn.getScene().getWindow();
+    }
+
+    private void closeCurrentStage(){
+        Stage currentStage = this.getCurrentStage();
+        currentStage.close();
+    }
+
 }
+
--- src/main/java/com/lap/lapproject/controller/TrainerController.java
+++ src/main/java/com/lap/lapproject/controller/TrainerController.java
@@ -1,16 +1,27 @@
 package com.lap.lapproject.controller;
 
+import com.lap.lapproject.LoginApplication;
 import com.lap.lapproject.application.Constants;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 import static com.lap.lapproject.controller.BaseController.model;
 
 public class TrainerController {
 
     @FXML
+    private Button addTrainerBtn;
+
+    @FXML
     private void onAddTrainerBtnClick(ActionEvent actionEvent) {
-        model.setPathForDetailView(Constants.PATH_TO_FXML_CREATE_NEW_TRAINER);
+        openNewWindow();
+        //model.setPathForDetailView(Constants.PATH_TO_FXML_CREATE_NEW_TRAINER);
     }
 
     @FXML
@@ -20,4 +31,27 @@ public class TrainerController {
     @FXML
     private void onSettingsBtnClick(ActionEvent actionEvent) {
     }
+
+
+    private Stage getCurrentStage(){
+        return (Stage) addTrainerBtn.getScene().getWindow();
+    }
+
+    private void openNewWindow(){
+        Stage stage = new Stage();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource(Constants.PATH_TO_FXML_CREATE_NEW_TRAINER));
+        Scene scene = null;
+
+        try {
+            scene= new Scene(fxmlLoader.load());
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        stage.setTitle("Trainer hinzufügen");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
