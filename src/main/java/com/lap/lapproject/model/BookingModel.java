e0bc01ffdb20b4e3c6e5d844287ed3a1d8841924
NepaConfus <tina.thurner@gmx.net>
2022-07-20 22:49:17 +0200

    search funktion is done yippie +BookingModel

--- src/main/java/com/lap/lapproject/model/BookingModel.java
+++ src/main/java/com/lap/lapproject/model/BookingModel.java
@@ -6,6 +6,7 @@ import com.calendarfx.model.Entry;
 import com.calendarfx.model.Interval;
 import com.calendarfx.view.CalendarView;
 import com.lap.lapproject.repos.BookingRepositoryJDBC;
+import com.lap.lapproject.repos.RoomRepositoryJDBC;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class BookingModel {
     Model model;
@@ -27,12 +29,11 @@ public class BookingModel {
         return calendarSource;
     }
 
-    public List<Entry<?>> allEntriesInInterval(LocalDate starttime, LocalDate endtime){
+    public List<Entry<?>> allEntriesInInterval(LocalDate startDate, LocalDate endDate) {
         List<Entry<?>> invervalEntries = new ArrayList<>();
-        for (Calendar calendar : calendarSource.getCalendars()){
-            Map<LocalDate, List<Entry<?>>> result = calendar.findEntries(starttime, endtime, ZoneId.systemDefault());
-            for (LocalDate key : result.keySet()){
-                result.get(key);
+        for (Calendar calendar : calendarSource.getCalendars()) {
+            Map<LocalDate, List<Entry<?>>> result = calendar.findEntries(startDate, endDate, ZoneId.systemDefault());
+            for (LocalDate key : result.keySet()) {
                 invervalEntries.addAll(result.get(key));
 
                 //if (result.get(0).get(0).getUserObject()instanceof Booking b){
@@ -43,31 +44,34 @@ public class BookingModel {
         return invervalEntries;
     }
 
-    public boolean findBookedRooms(List<Entry<?>> intervalEntries, LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime){
-        List<Entry<?>> bookedEntries = new ArrayList<>();
-        LocalDateTime start = LocalDateTime.of(startDate, startTime);
-        LocalDateTime end = LocalDateTime.of(endDate, endTime);
-        for (Entry entry : intervalEntries){    //TODO: Mit Räume vergleichen, Mit Tag vergleichen
-            if (entry.getUserObject()instanceof Booking b){;
-                if ((start.isBefore(b.getDateTimeStart()) && end.isBefore(b.getDateTimeStart()))
-                        || end.isEqual(b.getDateTimeEnd()) || start.isEqual(b.getDateTimeEnd()) || start.equals(b.getDateTimeEnd())){
-                    return true;
-                } else {
-                    return false; //TODO: Liste aller Räume machen
-                }
-            }
-        }
-        return false;
+    public List<Entry<?>> allEntriesInInterval(LocalDate date) {
+        return allEntriesInInterval(date, date);
     }
 
-    public List<Room> actualEmptyRooms(List<Entry<?>> intervalEntries, List<Entry<?>> bookedEntries){
-        List<Room> emptyRooms = new ArrayList<>();
-            for (Entry entry : intervalEntries){
-                if (true){
-                    //TODO: Logik
-                }
+
+    public List<Room> findEmptyRooms(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+
+        List<Entry<?>> intervalEntries = allEntriesInInterval(startDate, endDate);
+
+        RoomRepositoryJDBC roomsRepo = new RoomRepositoryJDBC();
+        List<Room> rooms = roomsRepo.readAll();
+
+        for (Entry entry :
+                intervalEntries) {
+            LocalTime startTimeOfEntry = entry.getInterval().getStartTime();
+            LocalTime endTimeOfEntry = entry.getInterval().getEndTime();
+            Booking entryOfUserObject = (Booking) entry.getUserObject();
+
+            Room room = entryOfUserObject.getRoom();
+
+            if (!((startTime.isBefore(startTimeOfEntry) && endTime.isBefore(startTimeOfEntry)
+                    || startTime.isAfter(endTimeOfEntry) && endTime.isAfter(endTimeOfEntry)
+                    || startTime.equals(endTimeOfEntry) || endTime.equals(startTimeOfEntry)
+            ))) {
+                rooms.remove(room);
             }
-            return emptyRooms;
+        }
+        return rooms;
     }
 
     /**
@@ -76,7 +80,7 @@ public class BookingModel {
      * Die sich wiederholenden Kurse werden mit Regex in einen lesbaren String gewandelt, um diese im Kalender anzuzeigen.
      * Wenn der Kurs nur einmalig ist, wird er ohne umwandlung in den Kalender eingetragen, da das Startdatum auch das Enddatum ist.
      */
-    public void loadBookingIntoCalendar(){
+    public void loadBookingIntoCalendar() {
 
         calendarSource = new CalendarSource("My Calendars");
 
@@ -92,22 +96,25 @@ public class BookingModel {
             System.out.println(course.getCourseName());
             Calendar newCalendar = new Calendar(currentCourse);
             model.bookings.forEach(booking -> {
-                if (booking.getCourse().getCourseName().equals(currentCourse)){
+                if (booking.getCourse().getCourseName().equals(currentCourse)) {
                     Entry<Booking> newEntry = new Entry<>(booking.getCourse().getProgram().getProgramName());
                     newEntry.setLocation(booking.getRoom().getLocation().getStreet());
                     newEntry.setUserObject(booking);
-                    if (!booking.getRecurrenceRule().equals("keiner")){
+                    if (!booking.getRecurrenceRule().equals("keiner")) {
                         BookingRepositoryJDBC bookingRepositoryJDBC = new BookingRepositoryJDBC();
+
+                        //TODO: frage wegen dailytime , daylytime ist anfangstag und anfangszeit
+                        // sollte es nicht ein endOfdaylyTime geben für das interval
+
                         LocalDateTime dailytime = LocalDateTime.of(booking.getDateTimeStart().toLocalDate(), booking.getDateTimeEnd().toLocalTime());
                         Interval interval = new Interval(booking.getDateTimeStart(), dailytime, ZoneId.systemDefault());
                         newEntry.setInterval(interval);
                         String recurrenceRule = bookingRepositoryJDBC.convertRecurrenceRuleFromTextToFrequency(booking.getRecurrenceRule());
-                        String regexDate = booking.getDateTimeEnd().toString().replaceAll("[-:]","").substring(0,8);
+                        String regexDate = booking.getDateTimeEnd().toString().replaceAll("[-:]", "").substring(0, 8);
                         newEntry.setRecurrenceRule(recurrenceRule + ";UNTIL=" + regexDate + ";");
                     } else {
                         Interval interval = new Interval(booking.getDateTimeStart(), booking.getDateTimeEnd(), ZoneId.systemDefault());
                         newEntry.setInterval(interval);
-
                     }
                     newCalendar.addEntry(newEntry);
                 }
